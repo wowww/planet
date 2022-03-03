279ba244e0e39f5033138f7912105f10fdc202fd
Eunjee <58850132+wowww@users.noreply.github.com>
2022-03-03 10:07:40 +0900

    docs. Sphere(구) 반응형 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import { Canvas } from "@react-three/fiber";
+import { useMediaQuery } from "react-responsive";
 
 function Sphere() {
+  const isPc = useMediaQuery({
+    query : "(min-width:1024px)"
+  });
+  const isTablet = useMediaQuery({
+    query : "(min-width:768px) and (max-width:1023px)"
+  });
+  const isMobile = useMediaQuery({
+    query : "(max-width:767px)"
+  });
+
   return (
     <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
     <directionalLight intensity={0.5} />
-    <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+    {isPc && <sphereGeometry attach="geometry" args={[2, 32, 32]} />}
+    {isTablet && <sphereGeometry attach="geometry" args={[1.5, 32, 32]} />}
+    {isMobile && <sphereGeometry attach="geometry" args={[1, 32, 32]} />}
+    
     <meshBasicMaterial
       attach="material"
       color="white"
