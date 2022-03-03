9fb567e62d83291d5bb31d9340d0015fd8da2397
Eunjee <58850132+wowww@users.noreply.github.com>
2022-03-03 09:44:37 +0900

    docs. 캔버스 안에 Sphere(구)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+
+function Sphere() {
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
+    <directionalLight intensity={0.5} />
+    <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+    <meshBasicMaterial
+      attach="material"
+      color="white"
+      roughness={0.1}
+      metalness={1}
+    />
+  </mesh>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return ( 
+    <Canvas>
+      <Sphere />
+    </Canvas>
+  )
 }
 
 export default App;
